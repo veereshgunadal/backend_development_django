9517f35ae18671684a00ff85a86eda3a5f8650ec
Veeresh Gunadal <veeresh.gunadal@ibm.com>
2026-03-14 14:26:42 +0530

    alpha dev1 developed feature1 get api

--- simple_crud/app/views.py
+++ simple_crud/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
+import json
 # Create your views here.
+
+def get(request):
+    response = json.dumps({'method':'get', 'code':200, 'message':'i am get api'})
+    return JsonResponse({'method':'get', 'code':200, 'message':'i am get api'})
+    #return HttpResponse(response)
